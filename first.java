d68e3ecf8726751345c25ea8b126d24344883280
babita7056 <babita7056.ca22@chitkara.edu.in>
2025-08-28 10:37:47 +0530

    add of number

--- first.java
+++ first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("hello world");
+       int a=5;
+       int b=6;
+       int c=a+b;
+       System.out.println(c);
     }
 }
