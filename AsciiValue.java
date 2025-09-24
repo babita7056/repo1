cdfbbf034e241dbf8c9caece0108052732a7acc5
babita7056 <161450098+babita7056@users.noreply.github.com>
2025-09-24 14:32:15 +0530

    Delete AsciiValue.java

--- AsciiValue.java
+++
@@ -1,9 +0,0 @@
-//5. Write a program to find the ASCII value of a character.
-public class AsciiValue {
-    public static void main(String[] args) {
-        char c = 'A'; 
-        int ascii = c; 
-        System.out.println("ASCII value of " + c + " = " + ascii);
-    }
-}
-
