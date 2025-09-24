4cb0b719084211b8accc0e8f0ca89dd6935216f9
babita7056 <161450098+babita7056@users.noreply.github.com>
2025-09-24 14:37:39 +0530

    Delete MemoryAllocation.java

--- MemoryAllocation.java
+++
@@ -1,9 +0,0 @@
-//15. Write a program to show memory allocation difference between primitive and reference variables.
-public class MemoryAllocation {
-    public static void main(String[] args) {
-        int x = 10;                 
-        int[] arr = {1, 2, 3};     
-        System.out.println("Primitive int x = " + x);
-        System.out.println("Reference variable arr points to heap: " + arr);
-    }
-}
