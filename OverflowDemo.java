cf0707a36da0be3edd647f2acb9153161eabeaf0
babita7056 <161450098+babita7056@users.noreply.github.com>
2025-09-24 14:38:20 +0530

    Delete OverflowDemo.java

--- OverflowDemo.java
+++
@@ -1,8 +0,0 @@
-//18. Write a program to demonstrate numeric overflow in Java
-public class OverflowDemo {
-    public static void main(String[] args) {
-        int max = Integer.MAX_VALUE; 
-        System.out.println("Max int = " + max);
-        System.out.println("After overflow = " + (max + 1));
-    }
-}
