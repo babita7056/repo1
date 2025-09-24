40465867314d5c90fd35d103bab17ee8396a3e18
babita7056 <161450098+babita7056@users.noreply.github.com>
2025-09-24 14:35:33 +0530

    Delete CharVsString.java

--- CharVsString.java
+++
@@ -1,9 +0,0 @@
-//11. Write a program to show the difference between char and String data types.
-public class CharVsString {
-    public static void main(String[] args) {
-        char c = 'A';          
-        String s = "Hello";    
-        System.out.println("char value: " + c);
-        System.out.println("String value: " + s);
-    }
-}
